5426385a69ed06f61255e8060e245f5fdf7333e4
rouibi <ilyes.rouibi@epfl.ch>
2024-02-20 13:18:14 +0100

    3.5 (à revoir de ton côté)

--- src/ch/epfl/chacun/Rotation.java
+++ src/ch/epfl/chacun/Rotation.java
@@ -2,21 +2,53 @@ package ch.epfl.chacun;
 
 import java.util.List;
 
+/**
+ * Enumeration representing different possible rotations.
+ */
 public enum Rotation {
     NONE,
     RIGHT,
     HALF_TURN,
     LEFT;
 
-
+    /**
+     * Immutable list of all rotations.
+     */
     public static final List<Rotation> ALL = List.of(values());
+
+    /**
+     * Total number of rotations.
+     */
     public static final int COUNT = ALL.size();
 
-    public Rotation add(Rotation that){
-        return ALL.get(this.ordinal() + that.ordinal());
+    /**
+     * Performs addition of two rotations.
+     *
+     * @param that The rotation to add to the current rotation.
+     * @return The resulting rotation.
+     */
+    public Rotation add(Rotation that) {
+        return ALL.get((this.ordinal() + that.ordinal()) % COUNT);
     }
 
-    public Rotation negated(){
+    /**
+     * @return The negated rotation.
+     */
+    public Rotation negated() {
+        return ALL.get((this.ordinal() + HALF_TURN.ordinal()) % COUNT);
+    }
 
+    /**
+     * @return The number of quarter turns clockwise.
+     */
+    public int quarterTurnsCW() {
+        return this.ordinal();
+    }
+
+    /**
+     * @return The angle in degrees clockwise.
+     */
+    public int degreesCW() {
+        return this.ordinal() * 90;
     }
 }
