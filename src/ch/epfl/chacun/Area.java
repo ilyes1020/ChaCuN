62ee3c13b0d141867c2fb5d085aecd3810644877
rouibi <ilyes.rouibi@epfl.ch>
2024-03-05 13:10:30 +0100

    3.1 jusque connectTo

--- src/ch/epfl/chacun/Area.java
+++ src/ch/epfl/chacun/Area.java
@@ -1,9 +1,6 @@
 package ch.epfl.chacun;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * record representing an area
@@ -23,50 +20,109 @@ public record Area<Z>(Set<Z> zones, List<PlayerColor> occupants, int openConnect
         occupants = List.copyOf(sortedOccupants);
     }
 
-//    public static boolean hasMenhir(Area<Zone.Forest> forest){
-//        return ;
-//    }
-//    public static int mushroomGroupCount(Area<Zone.Forest> forest){
-//        return ;
-//    }
-//    public static Set<Animal> animals(Area<Zone.Meadow> meadow, Set<Animal> cancelledAnimals){
-//        return ;
-//    }
-//    public static int riverFishCount(Area<Zone.River> river){
-//        return ;
-//    }
-//    public static int riverSystemFishCount(Area<Zone.Water> riverSystem){
-//        return ;
-//    }
-//    public static int lakeCount(Area<Zone.Water> riverSystem){
-//        return ;
-//    }
-//    public boolean isClosed(){
-//        return ;
-//    }
-//    public boolean isOccupied(){
-//        return ;
-//    }
-//    public Set<PlayerColor> majorityOccupants(){
-//        return ;
-//    }
-//    public Area<Z> connectTo(Area<Z> that){
-//        return ;
-//    }
-//    public Area<Z> withInitialOccupant(PlayerColor occupant){
-//        return ;
-//    }
-//    public Area<Z> withoutOccupant(PlayerColor occupant){
-//        return ;
-//    }
-//    public Area<Z> withoutOccupants(){
-//        return ;
-//    }
-//    public Set<Integer> tileIds(){
-//        return ;
-//    }
-//    public Zone zoneWithSpecialPower(Zone.SpecialPower specialPower){
-//        return ;
-//    }
+    public static boolean hasMenhir(Area<Zone.Forest> forest){
+        for(Zone.Forest aForest : forest.zones()){
+            if (aForest.kind() == Zone.Forest.Kind.WITH_MENHIR){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static int mushroomGroupCount(Area<Zone.Forest> forest){
+        int count = 0;
+        for(Zone.Forest aForest : forest.zones()){
+            if (aForest.kind() == Zone.Forest.Kind.WITH_MUSHROOMS){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static Set<Animal> animals(Area<Zone.Meadow> meadow, Set<Animal> cancelledAnimals){
+        Set <Animal> animals = new HashSet<>();
+        for(Zone.Meadow aMeadow : meadow.zones()){
+            animals.addAll(aMeadow.animals());
+        }
+        animals.removeAll(cancelledAnimals);
+        return animals;
+    }
+    public static int riverFishCount(Area<Zone.River> river) {
+        Set<Zone.Lake> lakesEncountered = new HashSet<>();
+        int fishCount = 0;
+
+        for (Zone.River aRiver : river.zones()) {
+            if (aRiver.hasLake()) {
+                Zone.Lake lake = aRiver.lake();
+                if (lakesEncountered.add(lake)) {
+                    fishCount += lake.fishCount();
+                }
+            } else {
+                fishCount += aRiver.fishCount();
+            }
+        }
+
+        return fishCount;
+    }
+
+    public static int riverSystemFishCount(Area<Zone.Water> riverSystem){
+        int fishCount = 0;
+        for(Zone.Water aWater : riverSystem.zones()){
+            fishCount++; // pas sur
+        }
+        return fishCount;
+    }
+    public static int lakeCount(Area<Zone.Water> riverSystem){
+        int lakeCount = 0;
+        for(Zone.Water aWater : riverSystem.zones()){
+            if (aWater instanceof Zone.Lake){
+                lakeCount++;
+            }
+        }
+        return lakeCount;
+    }
+    public boolean isClosed(){
+        return openConnections == 0;
+    }
+    public boolean isOccupied(){
+        return !occupants.isEmpty();
+    }
+    public Set<PlayerColor> majorityOccupants() {
+        int[] occupantsCount = new int[PlayerColor.values().length];
+
+        for (PlayerColor color : occupants) {
+            occupantsCount[color.ordinal()]++;
+        }
+
+        int maxOccupants = Arrays.stream(occupantsCount).max().orElse(0);
+
+        if (maxOccupants > 0) {
+            Set<PlayerColor> majorityColors = new HashSet<>();
+            for (PlayerColor color : PlayerColor.values()) {
+                if (occupantsCount[color.ordinal()] == maxOccupants) {
+                    majorityColors.add(color);
+                }
+            }
+            return majorityColors;
+        } else {
+            return Collections.emptySet();
+        }
+    }
+    public Area<Z> connectTo(Area<Z> that){
+        return ;
+    }
+    public Area<Z> withInitialOccupant(PlayerColor occupant){
+        return ;
+    }
+    public Area<Z> withoutOccupant(PlayerColor occupant){
+        return ;
+    }
+    public Area<Z> withoutOccupants(){
+        return ;
+    }
+    public Set<Integer> tileIds(){
+        return ;
+    }
+    public Zone zoneWithSpecialPower(Zone.SpecialPower specialPower){
+        return ;
+    }
 
 }
--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -33,9 +33,9 @@ public final class Submit {
     // CONFIGURATION
     // -------------
     // Jeton du premier membre du groupe
-    private static final String TOKEN_1 = "eip0AiNo";
+    private static final String TOKEN_1 = "aiQuie6i";
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
-    private static final String TOKEN_2 = "oi7oveeR";
+    private static final String TOKEN_2 = "";
     // Noms des éventuels fichiers Java additionnels à inclure (p.ex. "MyClass.java")
     private static final List<String> ADDITIONAL_FILES =
             List.of();
