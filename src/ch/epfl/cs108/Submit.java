3d576acc05bf966161c18a0dcbe0437ad03801dd
N4dZ <dingweifeng.dw@gmail.com>
2024-03-19 14:55:47 +0100

    token étape 5 ajouté

--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -33,9 +33,9 @@ public final class Submit {
     // CONFIGURATION
     // -------------
     // Jeton du premier membre du groupe
-    private static final String TOKEN_1 = "fuoTh2eS";
+    private static final String TOKEN_1 = "";
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
-    private static final String TOKEN_2 = "oek6Ejae";
+    private static final String TOKEN_2 = "eequ3aiK";
     // Noms des éventuels fichiers Java additionnels à inclure (p.ex. "MyClass.java")
     private static final List<String> ADDITIONAL_FILES =
             List.of();
